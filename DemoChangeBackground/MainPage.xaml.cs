92ed41483fac140a7a215666ab5d72ac06778369
amoraitis <tasosthira7@gmail.com>
2017-02-15 01:03:46 +0200

    Initial commit

---
+++ DemoChangeBackground/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace DemoChangeBackground
+{    
+    public sealed partial class MainPage : Page
+    {
+        private DispatcherTimer timer;
+        DateTime now;
+        DateTime clicked;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            //Documentation:https://docs.microsoft.com/en-us/uwp/api/windows.ui.xaml.dispatchertimer
+            timer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 0, 1) };
+            timer.Start();
+            //Sample:https://github.com/Microsoft/Windows-universal-samples/blob/master/Samples/Accelerometer/cs/Scenario3_Polling.xaml.cs
+            timer.Tick += onStop;
+        }
+        private void onStop(object sender, object e)
+        {            
+            ((DispatcherTimer)sender).Stop();
+            MyRespond.IsEnabled = true;
+            MyRespond.Visibility = Visibility.Visible;
+            now = DateTime.Now;
+        }
+
+        private void MyRespond_Click(object sender, RoutedEventArgs e)
+        {
+            clicked = DateTime.Now;
+            MainGrid.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+            var sum = clicked - now;
+            Information.Text = "You did "+sum.TotalSeconds.ToString()+" seconds to respond";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+*DemoChangeBackground*
+
+A simple UWP app using DispatcherTimer Class of System.Windows.Threading namespace of Windows Runtime 
+and Struct System.DateTime. The target is to take the reaction of the user in seconds.
